4f029479cb8a6a586e924ddd664347a478f23dd4
sannidhiteredesai <sannidhiteredesai@users.noreply.github.com>
2019-09-17 08:55:09 +0530

    Modified print message format and added consumer_timeout_ms

--- kafka_and_python/producer_consumer/serde/string_serde/consumer.py
+++ kafka_and_python/producer_consumer/serde/string_serde/consumer.py
@@ -7,11 +7,15 @@ string_deserializer = lambda x: x.decode('utf-8')
 # auto_offset_reset='earliest' will be used only for the first time when no messages have been
 # consumed yet using the given consumer group.
 # For the next consumptions the offset wil have been committed so the value of auto_offset_reset is not used
+# StopIteration if no message after 100secs by using consumer_timeout_ms=100000
 consumer = KafkaConsumer(topic, group_id='some_consumer_group',
                          bootstrap_servers=[BOOTSTRAP_SERVERS],
                          value_deserializer=string_deserializer,
-                         auto_offset_reset='earliest')
+                         auto_offset_reset='earliest',
+                         consumer_timeout_ms=100000)
 
 # This for loop will continuously wait for new messages
 for msg in consumer:
-    print (msg)
+    print("Consumed[%s-%d] %d: key=%s value=%s" % (msg.topic, msg.partition,
+                                         msg.offset, msg.key,
+                                         msg.value))
