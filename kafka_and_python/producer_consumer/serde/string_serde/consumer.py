85eb6ac5e85d16d6dacd2c5665126fe38180a731
sannidhiteredesai <sannidhiteredesai@users.noreply.github.com>
2019-09-19 22:37:45 +0530

    Changing auto_offset_reset to latest

--- kafka_and_python/producer_consumer/serde/string_serde/consumer.py
+++ kafka_and_python/producer_consumer/serde/string_serde/consumer.py
@@ -4,14 +4,14 @@ from config import *
 topic='mytopic'
 string_deserializer = lambda x: x.decode('utf-8')
 
-# auto_offset_reset='earliest' will be used only for the first time when no messages have been
-# consumed yet using the given consumer group.
-# For the next consumptions the offset wil have been committed so the value of auto_offset_reset is not used
+# auto_offset_reset will be used only for the first time when no messages have been
+# consumed yet using the consumer in consumer group.
+# For the next consumptions the offset will have been committed so the value of auto_offset_reset is not used
 # StopIteration if no message after 100secs by using consumer_timeout_ms=100000
 consumer = KafkaConsumer(topic, group_id='some_consumer_group',
                          bootstrap_servers=[BOOTSTRAP_SERVERS],
                          value_deserializer=string_deserializer,
-                         auto_offset_reset='earliest',
+                         auto_offset_reset='latest',
                          consumer_timeout_ms=100000)
 
 # This for loop will continuously wait for new messages
