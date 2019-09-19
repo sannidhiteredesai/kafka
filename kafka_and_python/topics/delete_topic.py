1d5bb6f235b6ea666d3723713c7fb5579fefa1c9
sannidhiteredesai <sannidhiteredesai@users.noreply.github.com>
2019-09-19 22:54:28 +0530

    Added some checks for topic is present or not

--- kafka_and_python/topics/delete_topic.py
+++ kafka_and_python/topics/delete_topic.py
@@ -4,10 +4,18 @@ from config import *
 
 delete_topic = "mytopic"
 admin_client = KafkaAdminClient(bootstrap_servers=BOOTSTRAP_SERVERS)
-admin_client.delete_topics(topics=[delete_topic])
+consumer = KafkaConsumer(bootstrap_servers=[BOOTSTRAP_SERVERS])
 
-topics = KafkaConsumer(bootstrap_servers=[BOOTSTRAP_SERVERS]).topics()
-if delete_topic not in topics:
-    print(f"Topic: {delete_topic} deleted successfully !!")
+if delete_topic in consumer.topics():
+    admin_client.delete_topics(topics=[delete_topic])
+
+    if delete_topic not in consumer.topics():
+        print(f"Topic: {delete_topic} deleted successfully !!")
+    else:
+        print(f"Unable to delete topic: {delete_topic}")
+
 else:
-    print(f"Unable to delete topic: {delete_topic}")
+    print(f"Topic: {delete_topic} not found !!")
+
+consumer.close()
+admin_client.close()
